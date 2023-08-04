6e898d5a04fbd7c6ab342943727b67097f25ef98
LucianoArmas <lucianoarmas11@gmail.com>
2023-08-04 00:51:53 -0300

    Deleted some files, and made some mods

---
+++ js/assets/scroll.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+  let lazyImages = [].slice.call(document.querySelectorAll(".lazy"));
+
+  if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver((entries, observer)=> {
+          entries.forEach(function(entry) {
+              if (entry.isIntersecting) {
+                  let lazyImage = entry.target;
+                  lazyImage.src = lazyImage.dataset.src;
+                  lazyImage.classList.remove("lazy");
+                  lazyImageObserver.unobserve(lazyImage);
+              }
+          });
+      });
+
+      lazyImages.forEach((lazyImage) => {
+          lazyImageObserver.observe(lazyImage);
+      });
+  } else {
+      // Fallback para navegadores sin Intersection Observer
+      lazyImages.forEach((lazyImage)=> {
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+      });
+  }
+});
